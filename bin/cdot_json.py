38bc901889b1d196198145225287b657623f4ae6
David Lawrence <davmlaw@gmail.com>
2022-02-02 16:46:00 +1030

    Use ijson to reduce RAM usage so we can run on small VM

--- bin/cdot_json.py
+++ bin/cdot_json.py
@@ -283,48 +283,46 @@ def convert_gene_pyreference_to_gene_version_data(gene_data: Dict) -> Dict:
 
 def combine_builds(args):
     print("combine_builds")
-    genome_build_data = {
-        "GRCh37": json.load(gzip.open(args.grch37)),
-        "GRCh38": json.load(gzip.open(args.grch38)),
+    genome_build_file = {
+        "GRCh37": gzip.open(args.grch37),
+        "GRCh38": gzip.open(args.grch38),
     }
-
-    all_transcript_ids = set()
-    for genome_build, data in genome_build_data.items():
-        # TODO: Check cdot versions
-        json_builds = data["genome_builds"]
-        if json_builds != [genome_build]:
-            raise ValueError(f"JSON file provided for {genome_build} needs to have only {genome_build} data (has {json_builds})")
-        all_transcript_ids.update(data["transcripts"].keys())
 
     urls_different_coding = defaultdict(list)
     transcripts = {}
-    for transcript_id in all_transcript_ids:
-        transcript_data = {}  # Latest transcript data, "genome_builds" will be overwritten at end
-        genome_builds = {}
-        for genome_build, data in genome_build_data.items():
-            if build_transcript := data["transcripts"].get(transcript_id):
-                if transcript_data:
-                    # Latest always used, but check existing - if codons are different old versions are wrong so remove
-                    for field in ["start_codon", "stop_codon"]:
-                        old = transcript_data.get(field)
-                        new = build_transcript.get(field)
-                        if old != new:  # Old relied on different codons so is obsolete
-                            for build_coordinates in genome_builds.values():
-                                url = build_coordinates["url"]
-                                urls_different_coding[url].append(transcript_id)
-                            genome_builds = {}
+    for genome_build, f in genome_build_file.items():
+        # TODO: Check cdot versions
+        json_builds = next(ijson.items(f, "genome_builds"))
+        if json_builds != [genome_build]:
+            raise ValueError(f"JSON file provided for {genome_build} needs to have only {genome_build} data (has {json_builds})")
 
-                transcript_data = build_transcript
-                genome_builds[genome_build] = build_transcript["genome_builds"][genome_build]
-        transcript_data["genome_builds"] = genome_builds
-        transcripts[transcript_id] = transcript_data
+        f.seek(0)  # Reset for next ijson call
+        for transcript_id, build_transcript in ijson.kvitems(f, "transcripts"):
+            existing_transcript = transcripts.get(transcript_id)
+            genome_builds = {}
+            if existing_transcript:
+                genome_builds = existing_transcript["genome_builds"]
+                # Latest always used, but check existing - if codons are different old versions are wrong so remove
+                for field in ["start_codon", "stop_codon"]:
+                    old = existing_transcript.get(field)
+                    new = build_transcript.get(field)
+                    if old != new:  # Old relied on different codons so is obsolete
+                        for build_coordinates in genome_builds.values():
+                            url = build_coordinates["url"]
+                            urls_different_coding[url].append(transcript_id)
+                        genome_builds = {}
+
+            genome_builds[genome_build] = build_transcript["genome_builds"][genome_build]
+            # Use latest (with merged genome builds)
+            build_transcript["genome_builds"] = genome_builds
+            transcripts[transcript_id] = build_transcript
 
     print("Writing cdot data")
     with gzip.open(args.output, 'w') as outfile:
         data = {
             "transcripts": transcripts,
             "cdot_version": cdot.__version__,
-            "genome_builds": list(genome_build_data.keys()),
+            "genome_builds": list(genome_build_file.keys()),
         }
         json_str = json.dumps(data)
         outfile.write(json_str.encode('ascii'))
