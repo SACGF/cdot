b48a26778a7bcf5f8589bbac02fd21108e91251e
David Lawrence <davmlaw@gmail.com>
2022-02-03 12:07:04 +1030

    Create all script

---
+++ generate_transcript_data/all_transcripts.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+CDOT_VERSION=0.2.1
+BASE_DIR=$(dirname ${BASH_SOURCE[0]})
+
+# RefSeq
+mkdir -p refseq
+cd refseq
+
+mkdir -p GRCh37
+cd GRCh37
+${BASE_DIR}/refseq_transcripts_grch37.sh
+cd ..
+
+mkdir -p GRCh38
+cd GRCh38
+${BASE_DIR}/refseq_transcripts_grch38.sh
+cd ..
+
+# Combine genome builds (we're in refseq dir)
+cdot_json.py combine_builds \
+    --grch37 GRCh37/cdot-${CDOT_VERSION}.refseq.grch37.json.gz \
+    --grch38 GRCh38/cdot-${CDOT_VERSION}.refseq.grch38.json.gz \
+    --output cdot-${CDOT_VERSION}.refseq.grch37_grch38.json.gz
+
+cd ..
+
+# RefSeq
+mkdir -p ensembl
+cd ensembl
+
+mkdir -p GRCh37
+cd GRCh37
+${BASE_DIR}/ensembl_transcripts_grch37.sh
+cd ..
+
+mkdir -p GRCh38
+cd GRCh38
+${BASE_DIR}/ensembl_transcripts_grch38.sh
+cd ..
+
+# Combine genome builds (we're in ensembl dir)
+cdot_json.py combine_builds \
+    --grch37 GRCh37/cdot-${CDOT_VERSION}.ensembl.grch37.json.gz \
+    --grch38 GRCh38/cdot-${CDOT_VERSION}.ensembl.grch38.json.gz \
+    --output cdot-${CDOT_VERSION}.ensembl.grch37_grch38.json.gz
+
+cd ..
