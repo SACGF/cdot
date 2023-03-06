035bd306c8ef829ba25d6d58f17b760be892e589
Dave Lawrence <davmlaw@gmail.com>
2023-03-06 11:57:30 +1030

    Add new Ensembl

--- generate_transcript_data/ensembl_transcripts_grch38.sh
+++ generate_transcript_data/ensembl_transcripts_grch38.sh
@@ -25,7 +25,7 @@ fi
 
 #81 is first GFF3 for GRCh38
 merge_args=()
-for release in 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108; do
+for release in 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100 101 102 103 104 105 106 107 108 109; do
   # Switched to using GTFs as they contain protein version
   filename=Homo_sapiens.GRCh38.${release}.gff3.gz
   url=ftp://ftp.ensembl.org/pub/release-${release}/gff3/homo_sapiens/${filename}
